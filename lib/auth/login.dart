7836bb9f952c61eda65af05c93ec21c3f5f74ec1
sujalR978 <srashiya955@rku.ac.in>
2026-08-27 11:50:01 +0530

    #

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -10,11 +10,45 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 80),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.black12, width: 1),
+                ),
+                child: CircleAvatar(
+                  radius: 64,
+                  backgroundImage: AssetImage('assets/image/L_image.png'),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 5),
+
+            Text(
+              "Examora",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+
+            SizedBox(height: 0),
+
+            Text(
+              "Smart Exams. Better Results.",
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 16,
+                color: Colors.black87,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
