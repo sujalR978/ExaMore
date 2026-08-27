7b828d0a70077acdf58297e3896cd3af7bc319bd
sujalR978 <srashiya955@rku.ac.in>
2026-08-27 15:44:30 +0530

    Add Filedtitle widget for customizable text display

---
+++ lib/features/auth/widgets/filedTitle.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Filedtitle extends StatefulWidget {
+  final String text;
+  const Filedtitle({super.key, required this.text});
+
+  @override
+  State<Filedtitle> createState() => _FiledtitleState();
+}
+
+class _FiledtitleState extends State<Filedtitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.text,
+      style: TextStyle(
+        fontWeight: FontWeight(600),
+        color: Color(0xff0B1C30),
+        fontSize: 14,
+      ),
+    );
+  }
+}
