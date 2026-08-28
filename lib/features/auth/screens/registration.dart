077d374bb53e6b042c28c978dd6e5dc29acbd923
sujalR978 <srashiya955@rku.ac.in>
2026-08-28 13:16:01 +0530

    Implement initial registration screen layout with styling

--- lib/features/auth/screens/registration.dart
+++ lib/features/auth/screens/registration.dart
@@ -10,6 +10,38 @@ class Registration extends StatefulWidget {
 class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50, bottom: 30),
+            child: Container(
+              width: 358,
+              
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 4),
+                    blurRadius: 12,
+                    color: Color(0xFF4C1D95).withValues(alpha: 0.04),
+                  ),
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(width: 1, color: Color(0xffE2E8F0)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32,bottom: 32),
+                child: Column(children: [
+                            
+                ],
+                            ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
