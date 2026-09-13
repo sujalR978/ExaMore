1ef437367e111685604b0c2d5443c25e39b2895a
sujalR978 <srashiya955@rku.ac.in>
2026-09-13 16:45:52 +0530

    #

--- lib/features/Admin/screen/supportScreen.dart
+++ lib/features/Admin/screen/supportScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:prep_mate/features/Admin/screen/adminHomeScreen.dart';
+
+import 'package:prep_mate/features/Admin/screen/adminMenuDrawer.dart';
 
 class SupportOverviewScreen extends StatefulWidget {
   const SupportOverviewScreen({super.key});
@@ -107,56 +108,7 @@ class _SupportOverviewScreenState extends State<SupportOverviewScreen> {
     return Scaffold(
       backgroundColor: theme.scaffoldBackgroundColor,
       key: _scaffoldKey,
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: isDarkMode
-                    ? const Color(0xFF1E1B4B)
-                    : const Color(0xFF1E1B4B),
-              ),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Examora Admin',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    'Navigation Menu',
-                    style: TextStyle(color: Color(0xFF94A3B8), fontSize: 13),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.dashboard_outlined),
-              title: const Text('Dashboard'),
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => AdminHomeScreen()),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.support_agent_outlined),
-              title: const Text('Support Overview'),
-              onTap: () => Navigator.pop(context),
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings_outlined),
-              title: const Text('Settings'),
-              onTap: () => Navigator.pop(context),
-            ),
-          ],
-        ),
-      ),
+     drawer: AdminMenuDrawer(),
       // Capsule-shaped Top Bar
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(75),
