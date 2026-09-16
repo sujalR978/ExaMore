74d1aa793c25e79d45979c8d6219a3db02cf6520
sujalR978 <srashiya955@rku.ac.in>
2026-09-16 10:51:21 +0530

    #

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -26,10 +26,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return android;
       case TargetPlatform.macOS:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for macos - '
@@ -60,4 +57,6 @@ class DefaultFirebaseOptions {
     projectId: 'examora-441d4',
     storageBucket: 'examora-441d4.firebasestorage.app',
   );
+
+  
 }
