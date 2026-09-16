06b0beeed230d7e43e0612489d82bc4ee9ed25f0
sujalR978 <srashiya955@rku.ac.in>
2026-09-16 10:51:25 +0530

    #

--- lib/features/auth/widgets/googleButton.dart
+++ lib/features/auth/widgets/googleButton.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:prep_mate/features/Auth/screens/login.dart';
+import 'package:prep_mate/features/Auth/services/auth_service.dart';
 
 class Googlebutton extends StatelessWidget {
   const Googlebutton({super.key});
@@ -16,11 +18,29 @@ class Googlebutton extends StatelessWidget {
               ? const Color(0xff475569)
               : const Color(0xffCBC4D2),
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
-      onPressed: () {},
+      onPressed: () async {
+        try {
+          final AuthService authService = AuthService();
+
+          await authService.singInWithGoogle();
+
+          if (!context.mounted) return;
+
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const Login()),
+          );
+        } catch (e) {
+          print("GOOGLE SIGN IN ERROR: $e");
+
+          if (context.mounted) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(e.toString())));
+          }
+        }
+      },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
